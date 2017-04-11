83dd46426d7b310909cbf3c52487e04180c20033
Liza-S <lizash605@gmail.com>
2017-04-11 22:37:11 +0300

    Clean up MediaPlayer resources according to activity lifecycle

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -76,6 +76,13 @@ public class NumbersActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // When the activity is stopped, release the media player resources because we won't be playing any more sounds.
+        releaseMediaPlayer();
+    }
+
     /**
      * Clean up the media player by releasing its resources.
      */
